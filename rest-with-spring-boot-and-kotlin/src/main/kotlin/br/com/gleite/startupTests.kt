71b26da15c3aad2fc48005d24ef0ac37addd6f85
lucah <gleite.lucas@gmail.com>
2024-11-15 15:28:03 -0300

    First steps in Kotlin with Spring Boot

---
+++ rest-with-spring-boot-and-kotlin/src/main/kotlin/br/com/gleite/startupTests.kt
@@ -0,0 +1,11 @@
+package br.com.gleite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWhitSpringBootAndKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWhitSpringBootAndKotlinApplication>(*args)
+}
